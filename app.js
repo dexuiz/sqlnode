3f3c493d5f1750a135f009585bf1e92a83aa53e7
dexuiz <dex345z@gmail.com>
2017-04-09 13:34:02 +0530

    commit

--- app.js
+++ app.js
@@ -7,20 +7,44 @@ app.set("view enigine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname +"/public"));
 
+var connection = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"secret",
+  database:"users",
+});
+
+connection.connect(function(err){
+  if(err){
+    console.log("error took place",err);
+  }
+});
+
+
 app.get("/",function(req,res){
   res.render("mainpage.login");
 });
 
 app.get("/register",function(req,res){
   res.render("register");
-})
+});
 
 app.get("/login",function(req,res){
   res.render("login");
-})
+});
 
 app.get("/zxcvf",function(req,res){
   res.render("admin");
+});
+
+app.post("/register",function(req,res){
+  connection.query("insert into users ?",req.body,function(error,results){
+    if(error){
+      console.log("error took place while insertion");
+    }else {
+      console.log(results)
+    }
+  })
 })
 
 
